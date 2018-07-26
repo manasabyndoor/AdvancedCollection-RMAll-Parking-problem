bfa9313e1f33746ddbde9cd70dc8b06545b84c9c
manasabyndoor <41484448+manasabyndoor@users.noreply.github.com>
2018-07-26 17:59:35 +0530

    Update Derived.java

--- SingleLvel/src/Derived.java
+++ SingleLvel/src/Derived.java
@@ -2,7 +2,7 @@ public class Derived extends Base{
 	int y;
 	Derived(int y)
 	{
-		super();
+		super();//invokes base class,x value will be retrieved
 		this.y=y;
 		System.out.println("DErived x="+x+"y="+y);
 	}
