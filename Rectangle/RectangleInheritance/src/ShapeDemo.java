5f10c2294fe5e628b0bbe697246e3629b39309a1
manasabyndoor <41484448+manasabyndoor@users.noreply.github.com>
2018-07-26 17:52:10 +0530

    Update ShapeDemo.java

--- Rectangle/RectangleInheritance/src/ShapeDemo.java
+++ Rectangle/RectangleInheritance/src/ShapeDemo.java
@@ -1,11 +1,11 @@
 
 public class ShapeDemo {
 	public static void main(String args[])
-	{
+	{	
 		Shape shape;
-		shape= new Circle();
+		shape= new Circle();//creating object for circle
 		shape.draw();
-		shape=new Rectangle();
+		shape=new Rectangle();//creating object for rectangle
 		shape.draw();
 		shape=new Shape();
 		shape.draw();
