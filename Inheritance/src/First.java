4b7247682a62ad82aac1735dabe9badb0d38b465
manasabyndoor <41484448+manasabyndoor@users.noreply.github.com>
2018-07-26 17:33:06 +0530

    Update First.java

--- Inheritance/src/First.java
+++ Inheritance/src/First.java
@@ -1,10 +1,10 @@
-
+//Parent
 public class First {
 
 	void First() {
 		int i;
 	}
-
+	//method to display
 	public void display() {
 		// TODO Auto-generated method stub
 		System.out.println("First");
