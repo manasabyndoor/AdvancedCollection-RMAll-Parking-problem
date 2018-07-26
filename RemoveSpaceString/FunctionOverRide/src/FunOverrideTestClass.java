e0442b53a11935d995fa3df7f23c9ee1310217de
manasabyndoor <41484448+manasabyndoor@users.noreply.github.com>
2018-07-26 17:56:33 +0530

    Update FunOverrideTestClass.java

--- RemoveSpaceString/FunctionOverRide/src/FunOverrideTestClass.java
+++ RemoveSpaceString/FunctionOverRide/src/FunOverrideTestClass.java
@@ -3,9 +3,9 @@ public class FunOverrideTestClass {
 	public static void main(String args[])
 	{
 	Parent parent=new Parent();
-	Parent p=new Child();//
-	p.disp();
-	parent.disp();
+	Parent p=new Child();
+	p.disp();//disp() method of child class is called 
+	parent.disp();//disp() method of parent class is called
 	}
 	
 
