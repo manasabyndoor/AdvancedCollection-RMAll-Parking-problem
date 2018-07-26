281c96a3487aa3187220682bb204425f6552c99e
manasabyndoor <41484448+manasabyndoor@users.noreply.github.com>
2018-07-26 17:18:45 +0530

    Add files via upload

---
+++ Complex.java
@@ -0,0 +1,25 @@
+//prigram to add complex numbers
+public class Complex {
+
+	float real;
+	float imaginary;
+	float sum;
+
+	public void set(float x, float y)// set the values
+	{
+		real = x;
+		imaginary = y;
+
+	}
+
+	void display() {
+		System.out.print(real);
+		System.out.println("+" + imaginary + "i");
+	}
+
+	public float add(float a, float b) {
+
+		return a + b;
+	}
+
+}
